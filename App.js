a7907516c7e8dec46a33baae7966c0894fea6cb0
kirk <kirk.douglas.richardson@gmail.com>
2017-10-19 21:14:22 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+// import library to help create Component
+import React from 'react';
+// import { Text } from 'react-native';
+import Header from './src/components/header.js';
+// create Component
+
+// render it to device
+
+const App = () => (
+  <Header />
+);
+
+
+export default App;
